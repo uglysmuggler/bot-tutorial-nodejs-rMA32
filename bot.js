78ffa42712886a3d1c2423c9259495a67c2f0adf
uglysmuggler1 <30273796+uglysmuggler1@users.noreply.github.com>
2017-07-18 17:23:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-var HTTPS = require('https');
+ var HTTPS = require('https');
 
 var cool = require('cool-ascii-faces');
 
@@ -18,7 +18,7 @@ function respond() {
 
       botRegexBM = /^\/beam/i; botRegexPR = /^\/rankings/; botRegexRK = /^\/rookies/; botRegexTL = /^\/teamlead/;
 
-      botRegexPL = /^\/playerlead/; botRegexBT = /^\/bot/;
+      botRegexPL = /^\/playerlead/; botRegexBT = /^\/bot/; botRegexLG = /^\/league/;
 
       
 
@@ -63,6 +63,15 @@ function respond() {
     this.res.writeHead(200);
 
     postMessage("http://www.freewebs.com/bradsdesigns/BotTutorial.html")
+
+    this.res.end();
+
+  }
+  else if(request.text && botRegexLG.test(request.text)) {
+
+    this.res.writeHead(200);
+
+    postMessage("http://www.daddyleagues.com/")
 
     this.res.end();
 
